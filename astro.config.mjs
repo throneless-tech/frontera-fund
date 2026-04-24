d537bb41070cf8ab2007d6fb97f0dbbee48afc13
Rae Gaines <rae@throneless.tech>
2026-04-24 11:25:39 -0400

    theme updates, integrating styles with cms content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import rehypeExternalLinks from "rehype-external-links";
