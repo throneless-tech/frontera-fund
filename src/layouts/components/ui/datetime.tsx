ec306ce7fd0ffadb16e5d04601b9dd3317c1d086
Rae Gaines <rae@throneless.tech>
2026-06-22 20:31:09 -0400

    update datepicker ts

--- src/layouts/components/ui/datetime.tsx
+++ src/layouts/components/ui/datetime.tsx
@@ -1,3 +1,4 @@
+import { setHours, setMinutes } from "date-fns";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 
@@ -7,12 +8,16 @@ import "react-datepicker/dist/react-datepicker.css";
 // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
 export function DateSelect(props: any) {
-  const [apptDate, setApptDate] = useState(new Date());
+  const [selectedDateTime, setSelectedDateTime] = useState<Date | null>(
+    setHours(setMinutes(new Date(), 30), 16),
+  );
+  
   return (
     <DatePicker
       showTimeSelect
-      selected={apptDate}
-      onChange={(date: any) => setApptDate(date)}
+      dateFormat="MMMM d, yyyy h:mm aa"
+      selected={selectedDateTime}
+      onChange={setSelectedDateTime}
       popperPlacement="bottom-end"
       popperClassName="datetime-popper"
     />
